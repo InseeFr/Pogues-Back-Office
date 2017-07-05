0614533a19d54640a07aa15b3359a419f02220a3
antoine cordier <com.acordier@gmail.com>
2017-07-05 16:09:10 +0200

    Set up Spring IOC container

--- src/main/java/fr/insee/pogues/persistence/service/QuestionnairesService.java
+++ src/main/java/fr/insee/pogues/persistence/service/QuestionnairesService.java
@@ -1,146 +1,18 @@
 package fr.insee.pogues.persistence.service;
 
-import fr.insee.pogues.persistence.query.EntityNotFoundException;
-import fr.insee.pogues.persistence.query.NonUniqueResultException;
-import fr.insee.pogues.persistence.query.QuestionnairesServiceQuery;
-import fr.insee.pogues.persistence.query.QuestionnairesServiceQueryPostgresqlImpl;
-import fr.insee.pogues.utils.json.JSONFunctions;
-import fr.insee.pogues.webservice.rest.PoguesException;
-import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
 import java.util.Map;
 
-
 /**
- * Questionnaire Service to assume the persistance of Pogues UI in JSON
- * 
- * @author I6VWID
- * 
- * @see /Pogues-BO/src/main/java/fr/insee/pogues/webservice/rest/
- *      PoguesPersistenceQuestionnaireList.java
- *
+ * Created by acordier on 05/07/17.
  */
-public class QuestionnairesService {
-
-	private QuestionnairesServiceQuery questionnaireServiceQuery;
-
-	final static Logger logger = Logger.getLogger(QuestionnairesService.class);
-
-	/**
-	 * Contructor for Questionnaires Service, init the transaction if needed
-	 * 
-	 */
-	public QuestionnairesService() {
-		// TODO use dependency injection
-		questionnaireServiceQuery = new QuestionnairesServiceQueryPostgresqlImpl();
-	}
-	
-	/**
-	 * A method to close the transaction if needed.
-	 * 
-	 */
-	public void close(){
-		questionnaireServiceQuery.close();
-	}
-
-	/**
-	 * A method to get the `QuestionnaireList` object in the database
-	 * 
-	 * @return the questionnaires list JSON description of the questionnaires
-	 */
-	public Map<String, JSONObject> getQuestionnaireList() throws Exception {
-		try {
-			return questionnaireServiceQuery.getQuestionnaires();
-		} catch(Exception e){
-			e.printStackTrace();
-			throw e;
-		}
-
-	}
-	
-	public Map<String, JSONObject> getQuestionnairesByOwner(String owner)throws Exception {
-		try {
-			return questionnaireServiceQuery.getQuestionnairesByOwner(owner);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-
-	}
-
-	/**
-	 * A method to get a `Questionnaire` object in the database given its ID
-	 *
-	 * @return the questionnaire matching the id
-	 */
-	public JSONObject getQuestionnaireByID(String id) throws Exception {
-		try {
-			JSONObject questionnaire = questionnaireServiceQuery.getQuestionnaireByID(id);
-			if(questionnaire.isEmpty()){
-				throw new PoguesException(404, "Not found", "Pas de questionnaire pour cet identifiant");
-			}
-			return questionnaire;
-		}  catch (NonUniqueResultException e) {
-			logger.error(e.getMessage());
-			throw e;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-
-	}
-	
-	public void deleteQuestionnaireByID(String id) throws Exception {
-		try {
-			questionnaireServiceQuery.deleteQuestionnaireByID(id);
-		} catch(Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-	
-	public void createQuestionnaire(JSONObject questionnaire) throws Exception {
-		try {
-			this.questionnaireServiceQuery.createQuestionnaire(questionnaire);
-		} catch(NonUniqueResultException e) {
-			logger.error(e.getMessage());
-			throw new PoguesException(400, "Bad request", e.getMessage());
-		} catch(Exception e){
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void updateQuestionnaire(JSONObject questionnaire) throws Exception {
-		try {
-			this.questionnaireServiceQuery.updateQuestionnaire(questionnaire);
-		} catch(EntityNotFoundException e) {
-			logger.error(e.getMessage());
-			throw new PoguesException(404, "Not found", e.getMessage());
-		} catch(Exception e){
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void createOrReplaceQuestionnaireList(String questionnaireList) {
-		
-		Map<String, String> questionnaires = JSONFunctions.getMap(questionnaireList);
-//		for(Entry<String, String> entry : questionnaires.entrySet()) {
-//			String id = entry.getKey();
-//			String questionnaire = entry.getValue();
-//			this.createOrReplaceQuestionnaire(id,questionnaire);
-//		}
-	}
-	
-	
-
-	
-	
-	
-	
-	
-	
-
+public interface QuestionnaireService {
+    void close();
+    Map<String, JSONObject> getQuestionnaireList() throws Exception;
+    Map<String, JSONObject> getQuestionnairesByOwner(String owner)throws Exception;
+    JSONObject getQuestionnaireByID(String id) throws Exception;
+    void deleteQuestionnaireByID(String id) throws Exception;
+    void createQuestionnaire(JSONObject questionnaire) throws Exception;
+    void updateQuestionnaire(JSONObject questionnaire) throws Exception;
 }
--- src/main/java/fr/insee/pogues/user/service/UserService.java
+++ src/main/java/fr/insee/pogues/user/service/UserService.java
@@ -1,76 +1,7 @@
 package fr.insee.pogues.user.service;
 
-import fr.insee.pogues.user.query.UserServiceQuery;
-import fr.insee.pogues.user.query.UserServiceQueryLDAPImpl;
-import fr.insee.pogues.utils.json.JSONFunctions;
-import org.json.simple.JSONArray;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Map;
-
 /**
- * User Service to assume the identity service of Pogues UI in JSON
- * 
- * @author I6VWID
- * 
- *
+ * Created by acordier on 05/07/17.
  */
-public class UserService {
-	
-	private HttpServletRequest request;
-	private UserServiceQuery serviceQuery;
-	
-	/**
-	 * Contructor for User Service
-	 * 
-	 */
-	public UserService(HttpServletRequest request){
-		this.request = request;
-		this.serviceQuery = new UserServiceQueryLDAPImpl();
-	}
-	
-	/**
-	 * A method to get the user id of the connected user
-	 * 
-	 * @return the user
-	 */
-	public String getUserID() {
-		
-		String id = request.getUserPrincipal().getName();
-		String json = "{\"id\":\""+id+"\"}";
-		
-		return json;
-
-	}
-	
-	/**
-	 * A method to get the user attributes of the connected user
-	 * 
-	 * @return the user
-	 */
-	public String getNameAndPermission() {
-		
-		String id = request.getUserPrincipal().getName();
-		Map<String, String> attributes = serviceQuery.getNameAndPermissionByID(id);
-		serviceQuery.close();
-		return JSONFunctions.getJSON(attributes);
-
-	}
-	/**
-	 * A method to list all available permissions
-	 *
-	 * @return the permissions list
-	 */
-	public List<String> getPermissions() throws Exception {
-		try {
-			return this.serviceQuery.getPermissions();
-		} catch(Exception e) {
-			throw e;
-		} finally {
-			this.serviceQuery.close();
-		}
-	}
-	
-	
+public interface UserService {
 }
