85620d168b1fa8209af55321a630486df4f7cd09
antoine cordier <com.acordier@gmail.com>
2017-07-19 17:18:42 +0200

    Fix Spring Security config

--- src/main/java/fr/insee/pogues/config/SecurityContext.java
+++ src/main/java/fr/insee/pogues/config/SecurityContext.java
@@ -29,7 +29,7 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login*").permitAll()
-                .anyRequest().fullyAuthenticated()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .usernameParameter("username")
