1e21f96a8e48ea12a86a0ef815c0117a7399f8ee
antoine cordier <com.acordier@gmail.com>
2017-07-19 17:18:21 +0200

    Update postgresql data

--- docker/postgresql/db/data.sql
+++ docker/postgresql/db/data.sql
@@ -1,2 +1,37 @@
 CREATE TABLE "pogues" (id VARCHAR(20), data JSON);
-INSERT INTO pogues VALUES ('jw4ctxyz', '{ "id": "jw4ctxyz", "owner": "DG75-L120"}');
+INSERT INTO pogues VALUES ('jw4ctxyz', '[
+	{
+		"owner": "DG75-L120",
+		"controls": [],
+		"agency": "fr.insee",
+		"componentGroups": {
+			"0": {
+				"name": "PAGE_1",
+				"id": "",
+				"label": "Components for page 1",
+				"Member": []
+			},
+			"id": "j5asdd8n"
+		},
+		"genericName": "QUESTIONNAIRE",
+		"codeLists": {
+			"codeListSpecification": [],
+			"codeList": []
+		},
+		"label": [
+			"Test Manu"
+		],
+		"type": "SequenceType",
+		"declarations": [],
+		"goTos": [],
+		"depth": 0,
+		"children": [],
+		"name": "manu",
+		"survey": {
+			"agency": "fr.insee",
+			"name": "POPO",
+			"id": ""
+		},
+		"id": "j5as8lkz"
+	}
+]');
