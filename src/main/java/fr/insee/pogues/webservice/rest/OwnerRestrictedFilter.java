cba77bf3a9ac0c5b70590efcf1893d384232aed8
antoine cordier <com.acordier@gmail.com>
2017-07-21 16:56:00 +0200

    Fix security issue in ACL Filter

--- src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
+++ src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
@@ -1,5 +1,6 @@
 package fr.insee.pogues.webservice.rest;
 
+import fr.insee.pogues.persistence.service.QuestionnairesService;
 import fr.insee.pogues.user.query.UserServiceQuery;
 import org.apache.log4j.Logger;
 import org.glassfish.jersey.server.ContainerRequest;
@@ -24,19 +25,33 @@ public class OwnerRestrictedFilter implements ContainerRequestFilter {
     @Autowired
     private UserServiceQuery userServiceQuery;
 
+    @Autowired
+    private QuestionnairesService questionnairesService;
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         try {
             ContainerRequest request = (ContainerRequest) requestContext;
             request.bufferEntity();
             JSONObject json = request.readEntity(JSONObject.class);
-            if(null == json){
-                // need a body to work
+            if (null == json) {
+                // need a body to continue
                 logger.warn("Tried to call OwnerRestricted filter on request without a body");
                 return;
             }
-            String owner = json.get("owner").toString();
-            if(null == owner){
+            Object id = json.get("id");
+            if(null == id){
+                // need an id to continue
+                logger.warn("Tried to call OwnerRestricted filter on body without an id");
+                return;
+            }
+            JSONObject questionnaire = questionnairesService.getQuestionnaireByID(id.toString());
+            if(questionnaire.isEmpty()){
+                // need an object to continue
+                return;
+            }
+            Object owner = questionnaire.get("owner");
+            if (null == owner) {
                 // need a owner attribute to work
                 logger.warn("Tried to call OwnerRestricted filter on body without a owner attribute");
                 return;
@@ -50,10 +65,13 @@ public class OwnerRestrictedFilter implements ContainerRequestFilter {
             String permission = userServiceQuery
                     .getNameAndPermissionByID(principal.getName())
                     .getPermission();
-            if (null == permission || !permission.equals(owner)) {
+            if (null == permission || !permission.equals(owner.toString())) {
                 throw new PoguesException(403, "Unauthorized", "This object is not yours");
             }
-        } catch(Exception e){
+        } catch (PoguesException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
             throw new PoguesException(500, "Unexpected error", e.getMessage());
         }
 
