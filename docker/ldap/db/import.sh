b84da1727b342af2a253a7450447431fae84c537
antoine cordier <com.acordier@gmail.com>
2017-07-25 19:20:15 +0200

    Fix travis build

--- docker/ldap/db/import.sh
+++ docker/ldap/db/import.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#env LDAPTLS_CACERT=/container/service/slapd/assets/certs/ca.crt
-
-#cat /container/service/slapd/assets/certs/ca.crt
-
-#sed -i 's/TLS_REQCERT demand/TLS_REQCERT allow/g' /etc/ldap/ldap.conf
-#
-#/etc/init.d/slapd restart
-
 sleep 2
 
 ldapmodify -x -D "cn=admin,cn=config" -w config -f /db/org.ldif
