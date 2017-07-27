0724a5f7c1574f24ee27607e3ef6515b1b49a2a0
antoine cordier <com.acordier@gmail.com>
2017-07-27 09:20:14 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
 function gitBook(){
+  echo "pushd docs"
   pushd docs
+  echo "gitbook install"
   gitbook install
+  echo "gitbook build"
   gitbook build
-  mv _book /tmp/_book
+  echo "mv _book /tmp/_book"
+  mv _book /tmp/_book$
+   echo "popd"
   popd
+   echo "GIT CHECKOUT ."
   git checkout .
+   echo "git checkout gh-pages"
   git checkout gh-pages
+    echo "MV /TMP/_BOOK/* ."
   mv /tmp/_book/* .
+   echo "GIT ADD ."
   git add .
+   echo "git commit"
   git commit -m "Travis Bot: Deploy docs for commit $TRAVIS_COMMIT"
+  echo "git push"
   git push
 }
 
