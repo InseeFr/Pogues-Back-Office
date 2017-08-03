3538f4b6c0309246dcf4fbe3c8fce983a5b1e09c
antoine cordier <com.acordier@gmail.com>
2017-08-03 14:02:34 +0200

    Fix: swagger base path

--- src/main/java/fr/insee/pogues/swagger/SwaggerConfig.java
+++ src/main/java/fr/insee/pogues/swagger/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package fr.insee.pogues.swagger;
 
 import io.swagger.jaxrs.config.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,7 +12,12 @@ import javax.servlet.http.HttpServlet;
 /**
  * Created by acordier on 24/07/17.
  */
+@PropertySource("classpath:${fr.insee.pogues.env:prod}/pogues-bo.properties")
 public class SwaggerConfig extends HttpServlet {
+
+    @Autowired
+    private Environment env;
+
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         BeanConfig beanConfig = new BeanConfig();
@@ -17,8 +25,8 @@ public class SwaggerConfig extends HttpServlet {
         beanConfig.setVersion("0.1");
         beanConfig.setDescription("The life and times of Pogues REST endpoints");
         beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("dvrmspogfolht01.ad.insee.intra/rmspogfo");
-        beanConfig.setBasePath("/pogues/");
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("rmspogfo/pogues/");
         beanConfig.setResourcePackage("fr.insee.pogues.webservice.rest");
         beanConfig.setScan(true);
         beanConfig.setDescription("Poges Backoffice API endpoints");
