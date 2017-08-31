31fadafe2c8b0c8d435fc55918f15de25ce90a36
Travis <travis@travis-ci.org>
2017-08-31 17:17:05 +0200

    Restrict deletion of a wrapping object to Owner

--- src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
+++ src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
@@ -34,29 +34,21 @@ public class OwnerRestrictedFilter implements ContainerRequestFilter {
         try {
             ContainerRequest request = (ContainerRequest) requestContext;
             request.bufferEntity();
-            JSONObject json = request.readEntity(JSONObject.class);
-            if (null == json) {
+            String id = requestContext.getUriInfo().getPathParameters().getFirst("id");
+            logger.debug("XXX id: " + id);
+            if (null == id) {
                 // need a body to continue
-                logger.error("Tried to call OwnerRestricted filter on request without a body");
-                throw new PoguesException(400, "Bad Request", "No body attached to request");
+                String message = "No id path param found to perform authorization";
+                logger.error(message);
+                throw new PoguesException(400, "Bad Request", message);
             }
-            Object id = json.get("id");
-            if(null == id){
-                // need an id to continue
-                logger.error("Tried to call OwnerRestricted filter on body without an id");
-                throw new PoguesException(400, "Bad Request", "Missing attribute 'id' in Request Payload");
-            }
-            JSONObject questionnaire = questionnairesService.getQuestionnaireByID(id.toString());
+            JSONObject questionnaire = questionnairesService.getQuestionnaireByID(id);
             if(questionnaire.isEmpty()){
                 // need an object to continue
                 throw new PoguesException(404, "Not Found", "Entity not found @id: " + id);
             }
             Object owner = questionnaire.get("owner");
-            if (null == owner) {
-                // need a owner attribute to work
-                logger.error("Tried to call OwnerRestricted filter on body without a owner attribute");
-                throw new PoguesException(400, "Bad Request", "Request payload does not have an owner attribute");
-            }
+            logger.debug("XXX questionnaire owner: " + owner);
             // Filter request
             Principal principal = requestContext.getSecurityContext()
                     .getUserPrincipal();
@@ -66,6 +58,7 @@ public class OwnerRestrictedFilter implements ContainerRequestFilter {
             String permission = userServiceQuery
                     .getNameAndPermissionByID(principal.getName())
                     .getPermission();
+            logger.debug("XXX user permission: " + permission);
             if (null == permission || !permission.equals(owner.toString())) {
                 throw new PoguesException(403, "Unauthorized", "This object is not yours");
             }
