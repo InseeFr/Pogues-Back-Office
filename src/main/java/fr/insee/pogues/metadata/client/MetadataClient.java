d2945d9c0566ac3d961f87097960abc445c63d63
a-cordier <com.acordier@gmail.com>
2017-09-14 17:16:52 +0200

    Use Rest Template in Metadata client

--- src/main/java/fr/insee/pogues/metadata/client/MetadataClient.java
+++ src/main/java/fr/insee/pogues/metadata/client/MetadataClient.java
@@ -1,45 +1,13 @@
 package fr.insee.pogues.metadata.client;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import fr.insee.pogues.metadata.model.ColecticaItem;
+import fr.insee.pogues.metadata.model.ColecticaItemRefList;
+
+import java.util.List;
 
 public interface MetadataClient {
 
-    /**
-     * Get an item from its ID
-     * @param id - Item identifier (Colectica Identifier attribute)
-     * @return
-     * @throws Exception
-     */
-    JSONObject getItem(String id) throws Exception;
-
-    /**
-     * Get all nested referenced items for a given ID
-     * @param id
-     * @return
-     * @throws Exception
-     */
-    JSONArray getChildren(String id) throws Exception;
-
-    /**
-     * Get all nested referenced items for a given ID
-     * itemQuery JSON reprezentation :
-     * <pre>
-     *     <code>
-     * {
-     *  "Identifiers": [
-     *          {
-     *              "AgencyId": "string",
-     *              "Identifier": "string",
-     *              "Version": 0
-     *          }
-     *      ]
-     *  }
-     *     </code>
-     * </pre>
-     * @param itemsQuery
-     * @return
-     * @throws Exception
-     */
-    JSONArray getItems(JSONObject itemsQuery) throws Exception;
+    ColecticaItem getItem(String id) throws Exception;
+    List<ColecticaItem> getItems(ColecticaItemRefList query) throws Exception;
+    ColecticaItemRefList getChildrenRef(String id) throws Exception;
 }
