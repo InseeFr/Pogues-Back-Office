61c28666c3a200d75ecd615d42db0ae75b8c1b35
a-cordier <com.acordier@gmail.com>
2017-09-18 11:32:50 +0200

    Update mock data

--- docker/postgresql/db/data.sql
+++ docker/postgresql/db/data.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "pogues" (id VARCHAR PRIMARY KEY, data jsonb);
 -- Initialize ddi root ids
 CREATE TABLE "ddi_group" (id VARCHAR PRIMARY KEY);
-INSERT INTO "ddi_group" VALUES ('391e505c-dc05-4042-8b9d-c602ff72690d');
+INSERT INTO "ddi_group" VALUES ('5696739a-751c-4603-a0bc-e4c05bd41c83');
