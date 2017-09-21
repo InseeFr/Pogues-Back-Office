2e112428efb47b933da41014f44bd19bc145bd51
a-cordier <com.acordier@gmail.com>
2017-09-21 18:35:31 +0200

    Fix apply level addition in metadata hierarchy to ES imports

--- src/main/java/fr/insee/pogues/search/source/ColecticaSourceImporterImpl.java
+++ src/main/java/fr/insee/pogues/search/source/ColecticaSourceImporterImpl.java
@@ -22,12 +22,12 @@ public class ColecticaSourceImporterImpl implements ColecticaSourceImporter {
     @Autowired
     SearchService searchService;
 
-    List<String> groupIds;
+    List<String> rootIds;
 
     @PostConstruct
     public void setUp() throws Exception {
         try {
-            groupIds = metadataService.getGroupIds();
+            rootIds = metadataService.getGroupIds();
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
@@ -37,44 +37,48 @@ public class ColecticaSourceImporterImpl implements ColecticaSourceImporter {
 
     @Override
     public void source() throws Exception {
-        for (String id : groupIds) {
-            logger.debug("Getting data from colectica API for group " + id);
-            Group f = metadataService.getGroup(id);
-            searchService.save("group", f);
-            saveSeries(f.getSubGroups());
+        for (String id : rootIds) {
+            logger.debug("Getting data from colectica API for root id " + id);
+            PoguesItem r = metadataService.getDDIRoot(id);
+            logger.debug("Root contains " + r.getChildren().size() + " groups");
+            for(PoguesItem g: r.getChildren()) {
+                System.out.println(g);
+                searchService.save("group", g);
+                saveSeries(g.getChildren());
+            }
         }
     }
 
-    public void saveSeries(List<SubGroup> subGroups) throws Exception {
-        for (SubGroup s : subGroups) {
+    public void saveSeries(List<PoguesItem> subGroups) throws Exception {
+        for (PoguesItem s : subGroups) {
             searchService.save("sub-group", s);
-            saveOperations(s.getStudyUnits());
+            saveOperations(s.getChildren());
         }
     }
 
-    public void saveOperations(List<StudyUnit> studyUnits) throws Exception {
-        for (StudyUnit o : studyUnits) {
+    public void saveOperations(List<PoguesItem> studyUnits) throws Exception {
+        for (PoguesItem o : studyUnits) {
             searchService.save("study-unit", o);
-            saveDataCollections(o.getDataCollections());
+            saveDataCollections(o.getChildren());
         }
     }
 
-    public void saveDataCollections(List<DataCollection> dataCollections) throws Exception {
-        for (DataCollection dc : dataCollections) {
+    public void saveDataCollections(List<PoguesItem> dataCollections) throws Exception {
+        for (PoguesItem dc : dataCollections) {
             searchService.save("data-collection", dc);
-            saveInstrumentSchemes(dc.getInstrumentSchemes());
+            saveInstrumentSchemes(dc.getChildren());
         }
     }
 
-    public void saveInstrumentSchemes(List<InstrumentScheme> instrumentSchemes) throws Exception {
-        for (InstrumentScheme i : instrumentSchemes) {
+    public void saveInstrumentSchemes(List<PoguesItem> instrumentSchemes) throws Exception {
+        for (PoguesItem i : instrumentSchemes) {
             searchService.save("instrument-scheme", i);
-            saveQuestionnaires(i.getInstruments());
+            saveQuestionnaires(i.getChildren());
         }
     }
 
-    public void saveQuestionnaires(List<Instrument> instruments) throws Exception {
-        for (Instrument q : instruments) {
+    public void saveQuestionnaires(List<PoguesItem> instruments) throws Exception {
+        for (PoguesItem q : instruments) {
             searchService.save("instrument", q);
         }
     }
