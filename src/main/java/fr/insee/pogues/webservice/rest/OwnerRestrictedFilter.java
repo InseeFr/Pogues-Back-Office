59ac5b340bf8e5486252a6d458631435a3806306
bwerquin <benoit.werquin@insee.fr>
2018-02-22 16:57:39 +0100

    Remove Anonymous filter for Owner Filter

--- src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
+++ src/main/java/fr/insee/pogues/webservice/rest/OwnerRestrictedFilter.java
@@ -35,44 +35,44 @@ public class OwnerRestrictedFilter implements ContainerRequestFilter {
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		if (authentication) {
-			try {
-				ContainerRequest request = (ContainerRequest) requestContext;
-				request.bufferEntity();
-				String id = requestContext.getUriInfo().getPathParameters().getFirst("id");
-				if (null == id) {
-					// need a body to continue
-					String message = "No id path param found to perform authorization";
-					logger.error(message);
-					throw new PoguesException(400, "Bad Request", message);
-				}
-				JSONObject questionnaire = questionnairesService.getQuestionnaireByID(id);
-				if (null == questionnaire) {
-					return;
-				}
-				Object owner = questionnaire.get("owner");
-				if (null == owner) {
-					// need a body to continue
-					String message = "Could not perform security checks: Entity does not contains a owner attribute";
-					logger.error(message);
-					throw new PoguesException(500, "Invalid Data", message);
-				}
-				// Filter request
-				Principal principal = requestContext.getSecurityContext().getUserPrincipal();
-				if (null == principal) {
-					throw new PoguesException(401, "Unauthenticated", "You are not logged in");
-				}
-				String permission = userServiceQuery.getNameAndPermissionByID(principal.getName()).getPermission();
-				if (null == permission || !permission.equals(owner.toString())) {
-					throw new PoguesException(403, "Unauthorized", "This object is not yours");
-				}
-			} catch (PoguesException e) {
-				throw e;
-			} catch (Exception e) {
-				e.printStackTrace();
-				logger.error(e.getMessage());
-				throw new PoguesException(500, "Unexpected error", e.getMessage());
+
+		try {
+			ContainerRequest request = (ContainerRequest) requestContext;
+			request.bufferEntity();
+			String id = requestContext.getUriInfo().getPathParameters().getFirst("id");
+			if (null == id) {
+				// need a body to continue
+				String message = "No id path param found to perform authorization";
+				logger.error(message);
+				throw new PoguesException(400, "Bad Request", message);
 			}
+			JSONObject questionnaire = questionnairesService.getQuestionnaireByID(id);
+			if (null == questionnaire) {
+				return;
+			}
+			Object owner = questionnaire.get("owner");
+			if (null == owner) {
+				// need a body to continue
+				String message = "Could not perform security checks: Entity does not contains a owner attribute";
+				logger.error(message);
+				throw new PoguesException(500, "Invalid Data", message);
+			}
+			// Filter request
+			Principal principal = requestContext.getSecurityContext().getUserPrincipal();
+			if (null == principal) {
+				throw new PoguesException(401, "Unauthenticated", "You are not logged in");
+			}
+			String permission = userServiceQuery.getNameAndPermissionByID(principal.getName()).getPermission();
+			if (null == permission || !permission.equals(owner.toString())) {
+				throw new PoguesException(403, "Unauthorized", "This object is not yours");
+			}
+		} catch (PoguesException e) {
+			throw e;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+			throw new PoguesException(500, "Unexpected error", e.getMessage());
 		}
+
 	}
 }
