b9608806743a8379692829df129a3ee70e0e04bb
Werquin Benoît <I6VWID@ad.insee.intra>
2018-07-17 15:04:50 +0200

    Update Eno version

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ POGUES_MODEL_VERSION="0.1"
 ENO_CORE_GROUP_ID="fr.insee.eno"
 ENO_CORE_URL="https://github.com/InseeFr/Eno.git"
 ENO_CORE_ARTIFACT_ID="eno-core"
-ENO_CORE_VERSION="1.0.0"
+ENO_CORE_VERSION="1.0.2"
 
 function install_pogues_model(){
     bash scripts/gh2mvn.sh  "$POGUES_MODEL_URL" "$GROUP_ID" "$POGUES_MODEL_ARTIFACT_ID" "$POGUES_MODEL_VERSION"
