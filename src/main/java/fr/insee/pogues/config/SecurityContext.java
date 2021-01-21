f3d1a124cc3cb12bd2cef84e9dd59114dbeaf2b2
Loïc Henninger <loic.henninger@insee.fr>
2021-01-21 11:11:02 +0100

    Bug fix

--- src/main/java/fr/insee/pogues/config/SecurityContext.java
+++ src/main/java/fr/insee/pogues/config/SecurityContext.java
@@ -45,7 +45,7 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
 			http.csrf().disable().authorizeRequests()
 					.antMatchers("/login*").permitAll()
 					.antMatchers("/error*").permitAll()
-					.antMatchers("/pogues/persistance/questionnaire/json-lunatic*").permitAll()
+					.antMatchers("/pogues/persistence/questionnaire/json-lunatic*").permitAll()
 					.anyRequest().authenticated().and().formLogin().usernameParameter("username")
 					.passwordParameter("password").loginPage("/login.jsp").loginProcessingUrl("/login")
 					.defaultSuccessUrl("/").failureUrl("/error.jsp");
