c4c2ae7ff7c9d9760ad7a82fef4e758848f75cb2
Loïc Henninger <loic.henninger@insee.fr>
2021-05-04 10:21:47 +0200

    Fix error on swagger.json loading

--- src/main/java/fr/insee/pogues/config/auth/security/OpenIDConnectSecurityContext.java
+++ src/main/java/fr/insee/pogues/config/auth/security/OpenIDConnectSecurityContext.java
@@ -44,7 +44,7 @@ public class OpenIDConnectSecurityContext extends KeycloakWebSecurityConfigurerA
 			http.antMatcher("/**").requiresChannel().anyRequest().requiresSecure();
 		http.sessionManagement().disable();
 		http.authorizeRequests().antMatchers("/api/init").permitAll().antMatchers("/swagger-ui/**").permitAll()
-				.antMatchers("/api/swagger.json").permitAll().anyRequest().authenticated();
+				.antMatchers("/pogues/swagger.json").permitAll().anyRequest().authenticated();
 	}
 
 	/**
