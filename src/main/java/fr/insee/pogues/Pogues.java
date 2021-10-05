9f2b6415de0763b5efa38f908ec4faeb2205b081
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-10-05 05:05:28 +0200

    Start application

---
+++ src/main/java/fr/insee/pogues/Pogues.java
@@ -0,0 +1,13 @@
+package fr.insee.pogues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pogues {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pogues.class, args);
+    }
+
+}
