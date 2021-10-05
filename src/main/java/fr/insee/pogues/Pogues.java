50113a8f44c99857a17e4b961c81c7fa21614a4e
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2021-10-05 09:22:59 +0200

    utilisation de @configurationproperties

--- src/main/java/fr/insee/pogues/Pogues.java
+++ src/main/java/fr/insee/pogues/Pogues.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class,
         SecurityFilterAutoConfiguration.class,
         KeycloakAutoConfiguration.class,
         WebMvcAutoConfiguration.class})
+@ConfigurationPropertiesScan
 public class Pogues {
 
     public static void main(String[] args) {
