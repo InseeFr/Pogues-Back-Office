03d2963ec51825b4024e7061adb06e1ffc9f2e02
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2022-10-03 11:41:58 +0200

    Show stack trace when error occurs in transform

--- src/main/java/fr/insee/pogues/webservice/rest/PoguesTransforms.java
+++ src/main/java/fr/insee/pogues/webservice/rest/PoguesTransforms.java
@@ -1,57 +1,8 @@
 package fr.insee.pogues.webservice.rest;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.PropertyException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-
-import fr.insee.pogues.transforms.visualize.*;
-import org.apache.commons.io.IOUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.eclipse.persistence.jaxb.MarshallerProperties;
-import org.eclipse.persistence.jaxb.UnmarshallerProperties;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import fr.insee.pogues.conversion.JSONSerializer;
-import fr.insee.pogues.model.CodeList;
-import fr.insee.pogues.model.ComponentType;
-import fr.insee.pogues.model.Questionnaire;
-import fr.insee.pogues.model.VariableType;
-import fr.insee.pogues.model.FlowControlType;
-import fr.insee.pogues.persistence.service.QuestionnairesService;
 import fr.insee.pogues.transforms.PipeLine;
 import fr.insee.pogues.transforms.Transformer;
+import fr.insee.pogues.transforms.visualize.*;
 import fr.insee.pogues.webservice.model.CaptureEnum;
 import fr.insee.pogues.webservice.model.ColumnsEnum;
 import fr.insee.pogues.webservice.model.OrientationEnum;
@@ -61,6 +12,23 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Main WebService class of the PoguesBOOrchestrator
@@ -110,9 +78,6 @@ public class PoguesTransforms {
 
 	@Autowired
 	PoguesJSONToPoguesJSONDeref jsonToJsonDeref;
-
-	@Autowired
-	QuestionnairesService questionnairesService;
 
 	private static final String CONTENT_DISPOSITION = "Content-Disposition";
 
@@ -334,7 +299,7 @@ public class PoguesTransforms {
 					.map(ddiToFo::transform, params, questionnaireName)
 					.map(foToPdf::transform, params, questionnaireName).transform();
 		} catch (Exception e) {
-			logger.error(e.getMessage());
+			logger.error(e.getMessage(),e);
 			throw new PoguesException(500, e.getMessage(), null);
 		}
 		File file = new File(filePath);
