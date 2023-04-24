2ef88c3d589dbb06847f197d86288bdd15cd7123
Nicolas Senave <nicolas.senave@gmail.com>
2023-04-24 18:54:49 +0200

    refactor(composition): chained steps class

--- src/main/java/fr/insee/pogues/transforms/visualize/composition/QuestionnaireComposition.java
+++ src/main/java/fr/insee/pogues/transforms/visualize/composition/QuestionnaireComposition.java
@@ -9,8 +9,27 @@ public class QuestionnaireComposition {
 
     private QuestionnaireComposition() {}
 
+    static class DeReferencingPipeline {
+        Questionnaire questionnaire;
+        Questionnaire referencedQuestionnaire;
+        private DeReferencingPipeline(Questionnaire questionnaire, Questionnaire referencedQuestionnaire) {
+            this.questionnaire = questionnaire;
+            this.referencedQuestionnaire = referencedQuestionnaire;
+        }
+        static DeReferencingPipeline start(Questionnaire questionnaire, Questionnaire referencedQuestionnaire) {
+            log.info("Starting de-referencing of questionnaire '{}' in questionnaire '{}'",
+                    referencedQuestionnaire.getId(), questionnaire.getId());
+            return new DeReferencingPipeline(questionnaire, referencedQuestionnaire);
+        }
+        DeReferencingPipeline then(CompositionStep compositionStep) {
+            compositionStep.apply(questionnaire, referencedQuestionnaire);
+            return this;
+        }
+    }
+
     /**
      * Replace referenced questionnaire by its component. Update elements that are impacted.
+     * Note: Component group is not updated since it is not used by eno generation.
      * @param questionnaire Referencing questionnaire.
      * @param referencedQuestionnaire Referenced questionnaire.
      * @throws DeReferencingException if an error occurs during de-referencing.
@@ -18,34 +37,18 @@ public class QuestionnaireComposition {
     public static void insertReference(Questionnaire questionnaire, Questionnaire referencedQuestionnaire)
             throws DeReferencingException {
         //
-        log.info("Starting de-referencing of questionnaire '{}' in questionnaire '{}'",
+        DeReferencingPipeline.start(questionnaire, referencedQuestionnaire)
+                .then(new UpdateReferencedVariablesScope())
+                .then(new InsertSequences())
+                .then(new InsertVariables())
+                .then(new InsertCodeLists())
+                .then(new UpdateFlowControlBounds())
+                .then(new InsertFlowControls())
+                .then(new UpdateIterationBounds())
+                .then(new InsertIterations());
+        //
+        log.info("Finished de-referencing of questionnaire '{}' in questionnaire '{}'",
                 referencedQuestionnaire.getId(), questionnaire.getId());
-
-        // Update the scope of the referenced questionnaire variables
-        new UpdateReferencedVariablesScope().apply(questionnaire, referencedQuestionnaire);
-
-        // Add sequences
-        new InsertSequences().apply(questionnaire, referencedQuestionnaire);
-
-        // Add variables
-        new InsertVariables().apply(questionnaire, referencedQuestionnaire);
-
-        // Add code lists
-        new InsertCodeLists().apply(questionnaire, referencedQuestionnaire);
-
-        // Update filters in referencing questionnaire
-        new UpdateFlowControlBounds().apply(questionnaire, referencedQuestionnaire);
-
-        // Add flow controls (filters)
-        new InsertFlowControls().apply(questionnaire, referencedQuestionnaire);
-
-        // Update loops in referencing questionnaire
-        new UpdateIterationBounds().apply(questionnaire, referencedQuestionnaire);
-
-        // Add iterations (loops)
-        new InsertIterations().apply(questionnaire, referencedQuestionnaire);
-
-        // Component group is not updated since it is not used by eno generation
     }
 
 }
