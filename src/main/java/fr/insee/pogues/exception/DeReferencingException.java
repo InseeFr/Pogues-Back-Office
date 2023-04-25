7d377b4384f968dd38b018152251756db1104da9
Nicolas Senave <nicolas.senave@gmail.com>
2023-04-25 12:00:49 +0200

    refactor(composition): refactor unit tests

--- src/main/java/fr/insee/pogues/exception/DeReferencingException.java
+++ src/main/java/fr/insee/pogues/exception/DeReferencingException.java
@@ -3,7 +3,7 @@ package fr.insee.pogues.exception;
 /**
  * Exception thrown if an error occurs during questionnaire de-referencing (composition feature).
  */
-public class DeReferencingException extends RuntimeException {
+public class DeReferencingException extends Exception {
 
     public DeReferencingException(String message, Exception e) {
         super(message, e);
