e130dce734d597e856a63062db51b1f46fffbd3a
Loïc Henninger <loic.henninger@insee.fr>
2023-08-10 11:37:51 +0200

    Resolve conflicts in metadataClient

--- src/main/java/fr/insee/pogues/metadata/client/MetadataClient.java
+++ src/main/java/fr/insee/pogues/metadata/client/MetadataClient.java
@@ -1,7 +1,12 @@
 package fr.insee.pogues.metadata.client;
 
+import fr.insee.pogues.exception.IllegalFlowControlException;
+import fr.insee.pogues.exceptions.PoguesClientException;
 import fr.insee.pogues.metadata.model.ColecticaItem;
 import fr.insee.pogues.metadata.model.ColecticaItemRefList;
+import fr.insee.pogues.metadata.model.Operation;
+import fr.insee.pogues.metadata.model.Operation;
+import fr.insee.pogues.metadata.model.Serie;
 import fr.insee.pogues.metadata.model.Unit;
 
 import java.util.List;
@@ -9,10 +14,23 @@ import java.util.List;
 public interface MetadataClient {
 
     ColecticaItem getItem(String id) throws Exception;
+    
     List<ColecticaItem> getItems(ColecticaItemRefList query) throws Exception;
+    
     ColecticaItemRefList getChildrenRef(String id) throws Exception;
+    
 	List<Unit> getUnits() throws Exception;
+	
     String getDDIDocument(String id) throws Exception;
+    
 	String getCodeList(String id) throws Exception;
+	
+	List<Serie> getSeries() throws Exception;
+	
+	List<Operation> getOperationsBySerieId(String id) throws PoguesClientException, IllegalFlowControlException.PoguesClientException;
+	
+	Serie getSerieById(String id) throws Exception;
+	
+	Operation getOperationById(String id) throws Exception;
 	
 }
--- src/main/java/fr/insee/pogues/metadata/client/MetadataClientImpl.java
+++ src/main/java/fr/insee/pogues/metadata/client/MetadataClientImpl.java
@@ -3,24 +3,34 @@ package fr.insee.pogues.metadata.client;
 import java.util.Arrays;
 import java.util.List;
 
+import fr.insee.pogues.exception.IllegalFlowControlException;
 import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import fr.insee.pogues.config.RemoteMetadata;
 import fr.insee.pogues.metadata.model.ColecticaItem;
 import fr.insee.pogues.metadata.model.ColecticaItemRefList;
+import fr.insee.pogues.exception.PoguesException;
+import fr.insee.pogues.metadata.model.Operation;
+import fr.insee.pogues.metadata.model.Serie;
 import fr.insee.pogues.metadata.model.Unit;
 
 @Service
 public class MetadataClientImpl implements MetadataClient {
+
+	@Value("${fr.insee.pogues.api.remote.metadata.url}")
+	String serviceUrl;
 
 	private RemoteMetadata remoteMetadata;
 
@@ -34,7 +44,7 @@ public class MetadataClientImpl implements MetadataClient {
 		this.restTemplate=restTemplateBuilder.build();
 	}
 
-	public ColecticaItem getItem(String id) throws Exception {
+	public ColecticaItem getItem(String id) {
         String url = String.format("%s/meta-data/item/%s", remoteMetadata.getUrl(), id);
         return restTemplate.getForObject(url, ColecticaItem.class);
     }
@@ -58,7 +68,7 @@ public class MetadataClientImpl implements MetadataClient {
         return response.getBody();
     }
 
-    public String getDDIDocument(String id) throws Exception {
+    public String getDDIDocument(String id) {
         String url = String.format("%s/meta-data/item/%s/ddi", remoteMetadata.getUrl(), id);
         ResponseEntity<String>  response;
         response = restTemplate
@@ -66,19 +76,55 @@ public class MetadataClientImpl implements MetadataClient {
         return response.getBody();
     }
     
-	public List<Unit> getUnits() throws Exception {
+	public List<Unit> getUnits()  {
 		String url = String.format("%s/meta-data/units", remoteMetadata.getUrl());
 		ResponseEntity<Unit[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Unit[].class);
 		return Arrays.asList(response.getBody());
 	}
 
 	@Override
-	public String getCodeList(String id) throws Exception {
+	public String getCodeList(String id)  {
 		String url = String.format("%s/meta-data/codeList/%s/ddi", remoteMetadata.getUrl(), id);
 		ResponseEntity<String> response;
 		response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
 		return response.getBody();
 	}
+	
+	@Override
+	public List<Serie> getSeries() {
+		String url = String.format("%s/gestion/series", serviceUrl);
+		ResponseEntity<Serie[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Serie[].class);
+		return Arrays.asList(response.getBody());
+	}
+	
+	@Override
+	public List<Operation> getOperationsBySerieId(String id) throws IllegalFlowControlException.PoguesClientException {
+		String url = String.format("%s/gestion/series/%s/operations", serviceUrl, id);
+		ResponseEntity<Operation[]> response;
+		response = restTemplate.exchange(url, HttpMethod.GET, null, Operation[].class);
+		if (response.getStatusCode() != HttpStatus.OK) {
+			throw new IllegalFlowControlException.PoguesClientException(response.getStatusCodeValue(),response.getStatusCode().toString(),"");
+		}
+		return Arrays.asList(response.getBody());
+	}
     
+	@Override
+	public Serie getSerieById(String id) {
+		String url = String.format("%s/gestion/serie/%s", serviceUrl, id);
+		ResponseEntity<Serie> response = restTemplate.exchange(url, HttpMethod.GET, null, Serie.class);
+		return response.getBody();
+	}
+	
+	@Override
+	public Operation getOperationById(String id) throws Exception {
+		String url = String.format("%s/gestion/operation/%s", serviceUrl, id);
+		try {
+			ResponseEntity<Operation> response = restTemplate.exchange(url, HttpMethod.GET, null, Operation.class);
+			return response.getBody();
+		} catch (HttpClientErrorException e) {
+			throw new PoguesException(e.getRawStatusCode(), HttpStatus.valueOf(e.getRawStatusCode()).getReasonPhrase(), e.getResponseBodyAsString());
+		}
+
+	}
     
 }
