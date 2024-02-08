611af82995c90db527791cc19013fba8688790b1
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-02-08 10:01:12 +0100

    chore: fix import

--- src/main/java/fr/insee/pogues/webservice/rest/RestExceptionHandler.java
+++ src/main/java/fr/insee/pogues/webservice/rest/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package fr.insee.pogues.webservice.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +12,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { PoguesException.class })
     public ResponseEntity<ApiError> handlePoguesException(PoguesException pe) {
         ApiError apiErrorResponse = new ApiError(pe.getStatus(), pe.getMessage(), pe.getDetails());
-        return new ResponseEntity<>(apiErrorResponse, org.springframework.http.HttpStatus.valueOf(pe.getStatus()));
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.valueOf(pe.getStatus()));
     }
     @ExceptionHandler(value = { Exception.class })
     public ResponseEntity<ApiError> handleOtherException(Exception e) {
         ApiError apiErrorResponse = new ApiError(500,e.getCause().getClass().getSimpleName(), e.getCause().getMessage());
-        return new ResponseEntity<>(apiErrorResponse, org.springframework.http.HttpStatus.valueOf(500));
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.valueOf(500));
     }
 }
