268e74588db67f93e107fa9c3e3d9e62219024f0
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-06-21 16:18:35 +0200

    chore: add webclient timeout
    - default 120 seconds

--- src/main/java/fr/insee/pogues/configuration/AppConfiguration.java
+++ src/main/java/fr/insee/pogues/configuration/AppConfiguration.java
@@ -9,9 +9,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 @EnableConfigurationProperties
@@ -23,13 +27,22 @@ public class AppConfiguration {
     @Autowired
     private ExchangeFilterFunction webClientTokenInterceptor;
 
+    /* Timeout value in seconds */
+    @Value("${feature.webclient.timeout}")
+    long webClientTimeout;
+
     @Bean
     public WebClient webClient(
             @Value("${feature.oidc.enabled}") boolean oidcEnabled,
             WebClient.Builder builder) {
+
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(webClientTimeout));
+
         builder
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
         if(oidcEnabled)  builder.filter(webClientTokenInterceptor);
         return builder.build();
     }
