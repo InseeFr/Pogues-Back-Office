b20fa1d929baf1b55bdb70665b3184c282e69bc1
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2025-05-07 15:08:44 +0200

    feat: improve rolling backup (#359)
    * feat: create sql procedure in db for clean versions
    
    * chore: add method to call new procedure
    
    * feat: crean schedule service for backup cleaning
    
    * bump: 4.15.0-SNAPSHOT.0
    
    * fix: default CRON (only from monday to friday at 23:00:00)
    
    * bump: 4.15.0-SNAPSHOT.1

---
+++ src/main/java/fr/insee/pogues/persistence/service/ScheduleCleanerService.java
@@ -0,0 +1,27 @@
+package fr.insee.pogues.persistence.service;
+
+
+import fr.insee.pogues.persistence.repository.QuestionnaireVersionRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class ScheduleCleanerService {
+
+    private QuestionnaireVersionRepository questionnaireVersionRepository;
+
+    public ScheduleCleanerService(QuestionnaireVersionRepository questionnaireVersionRepository){
+        this.questionnaireVersionRepository = questionnaireVersionRepository;
+    }
+
+    @Scheduled(cron = "${feature.database.rollingBackup.cron}", zone = "${application.timezoneId}")
+    public void scheduleQuestionnaireSaveCleaner(){
+        log.info("START -- cleaning questionnaire backups");
+        log.info("Keep the last backup for a day, and keep 10 last backup regardless of the day");
+        questionnaireVersionRepository.cleanVersions();
+        log.info("END -- questionnaire backups cleaned");
+    }
+}
