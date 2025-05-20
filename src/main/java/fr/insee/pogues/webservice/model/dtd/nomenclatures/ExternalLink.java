2e775ba2767e63e4a30bb8f2b550a25e5993511b
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-05-20 14:22:16 +0200

    chore: create nomenclature pojo for DTD (interface with UI)

---
+++ src/main/java/fr/insee/pogues/webservice/model/dtd/nomenclatures/ExternalLink.java
@@ -0,0 +1,16 @@
+package fr.insee.pogues.webservice.model.dtd.nomenclatures;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExternalLink {
+    private String urn;
+}
