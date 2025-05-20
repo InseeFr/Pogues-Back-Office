c3be19d70d1fc56d6beff915385e0a8c8faa116c
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-05-20 14:48:21 +0200

    test: nomeclatureService

---
+++ src/test/java/fr/insee/pogues/service/NomenclatureServiceTest.java
@@ -0,0 +1,40 @@
+package fr.insee.pogues.service;
+
+import fr.insee.pogues.model.*;
+import fr.insee.pogues.persistence.service.QuestionnairesService;
+import fr.insee.pogues.webservice.model.dtd.nomenclatures.ExtendedNomenclature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import static fr.insee.pogues.utils.Utils.loadQuestionnaireFromResources;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@ExtendWith(MockitoExtension.class)
+class NomenclatureServiceTest {
+
+    @Mock
+    QuestionnairesService questionnairesService;
+    private NomenclatureService nomenclatureService;
+
+    @BeforeEach
+    void initQuestionnaireService(){
+        nomenclatureService = new NomenclatureService(questionnairesService);
+        initMocks(this);
+    }
+
+    @Test
+    @DisplayName("Should get right NomenclatureDTD from questionnaire")
+    void shouldGetRightNomenclaturesFromQuestionnaire() throws Exception {
+        Questionnaire questionnaire = loadQuestionnaireFromResources("service/withAllNomenclatures.json");
+        List<ExtendedNomenclature> nomenclatures = nomenclatureService.getNomenclaturesDTD(questionnaire);
+        assertThat(nomenclatures).hasSize(29);
+        assertThat(nomenclatures.getFirst().getRelatedQuestionNames())
+                .containsExactly("N_1");
+    }
+}
