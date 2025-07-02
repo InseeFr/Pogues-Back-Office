097a64143abac29ad1afe17253ad642439637ba2
Nicolas Senave <nicolas.senave@gmail.com>
2025-07-02 11:33:50 +0200

    fix: call model cleaner in visualize endpoints

--- src/main/java/fr/insee/pogues/service/ModelCleaningService.java
+++ src/main/java/fr/insee/pogues/service/ModelCleaningService.java
@@ -2,7 +2,10 @@ package fr.insee.pogues.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import fr.insee.pogues.conversion.JSONDeserializer;
+import fr.insee.pogues.conversion.JSONSerializer;
 import fr.insee.pogues.model.Questionnaire;
+import fr.insee.pogues.transforms.visualize.ModelTransformer;
 import fr.insee.pogues.utils.PoguesDeserializer;
 import fr.insee.pogues.utils.PoguesSerializer;
 import fr.insee.pogues.utils.model.cleaner.ControlCriticityCleaner;
@@ -13,14 +16,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.JAXBException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Map;
 
 import static fr.insee.pogues.utils.json.JSONFunctions.jsonStringtoJsonNode;
 
 @Service
 @Slf4j
-public class ModelCleaningService {
+public class ModelCleaningService implements ModelTransformer {
 
     public JsonNode cleanModel(JsonNode jsonNodeQuestionnaire) throws JAXBException, UnsupportedEncodingException, JsonProcessingException {
         Questionnaire questionnaire = PoguesDeserializer.questionnaireToJavaObject(jsonNodeQuestionnaire);
@@ -35,4 +41,20 @@ public class ModelCleaningService {
                 new LoopMinMaxCleaner());
         modelCleaners.forEach(modelCleaner -> modelCleaner.apply(questionnaire));
     }
+
+    @Override
+    public ByteArrayOutputStream transform(InputStream inputStream, Map<String, Object> params, String surveyName)
+            throws Exception {
+
+        JSONDeserializer jsonDeserializer = new JSONDeserializer();
+        Questionnaire questionnaire = jsonDeserializer.deserialize(inputStream);
+
+        cleanModel(questionnaire);
+
+        JSONSerializer jsonSerializer = new JSONSerializer();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(jsonSerializer.serialize(questionnaire).getBytes());
+        return outputStream;
+    }
+
 }
