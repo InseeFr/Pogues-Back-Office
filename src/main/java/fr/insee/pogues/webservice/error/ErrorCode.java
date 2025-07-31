a5dbbff66a5c8fa059b29623fe8a99bca92723f2
Chloe Renaud <chloe.renaud@insee.fr>
2025-07-31 17:57:24 +0200

    feat: dedicated variable service to handle variables CRUD

--- src/main/java/fr/insee/pogues/webservice/error/ErrorCode.java
+++ src/main/java/fr/insee/pogues/webservice/error/ErrorCode.java
@@ -2,7 +2,9 @@ package fr.insee.pogues.webservice.error;
 
 public enum ErrorCode {
     CODE_LIST_RELATED_QUESTIONS_NAME("codelist:relatedquestions:name"),
-    CODE_LIST_NOT_FOUND("codelist:notfound");
+    CODE_LIST_NOT_FOUND("codelist:notfound"),
+    VARIABLE_NOT_FOUND("variable:notfound");
+
     public final String label;
 
     ErrorCode(String label) {
