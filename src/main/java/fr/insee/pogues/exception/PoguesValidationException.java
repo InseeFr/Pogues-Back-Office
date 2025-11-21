70fb55363d02e814562b123ffe96971c93535f76
Nicolas Sénave <59770457+nsenave@users.noreply.github.com>
2025-11-21 15:54:43 +0100

    feat(validation): mandatory mcq check (#393)

---
+++ src/main/java/fr/insee/pogues/exception/PoguesValidationException.java
@@ -0,0 +1,9 @@
+package fr.insee.pogues.exception;
+
+public class PoguesValidationException extends Exception {
+
+    public PoguesValidationException(String message) {
+        super(message);
+    }
+
+}
