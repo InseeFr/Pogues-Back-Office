ff15399c9e81edac2a53b3dc7a1b666db311efe9
RemiVerriez <remi.verriez@insee.fr>
2026-05-06 17:24:42 +0200

    feat: add entity for mapping codes list pogues registre

--- src/main/java/fr/insee/pogues/domain/entity/db/MappingCodesListRegistreDB.java
+++ src/main/java/fr/insee/pogues/domain/entity/db/MappingCodesListRegistreDB.java
@@ -1,4 +1,26 @@
 package fr.insee.pogues.domain.entity.db;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "mapping_codes_list_registre")
+@Getter
+@Setter
+@NoArgsConstructor
 public class MappingCodesListRegistreDB {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "pogues_codes_list_id", nullable = false, unique = true, updatable = false)
+    private String poguesCodesListId;
+
+    @Column(name = "registre_codes_list_id", nullable = false)
+    private UUID registreCodesListId;
 }
