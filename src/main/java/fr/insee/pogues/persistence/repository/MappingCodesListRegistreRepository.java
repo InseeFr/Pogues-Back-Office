83a68a006cf5a611a1efaed8e3e3e7a0a5a4cf70
RemiVerriez <remi.verriez@insee.fr>
2026-05-07 14:52:37 +0200

    feat: update mapping pogues Registre repository

--- src/main/java/fr/insee/pogues/persistence/repository/MappingCodesListRegistreRepository.java
+++ src/main/java/fr/insee/pogues/persistence/repository/MappingCodesListRegistreRepository.java
@@ -1,4 +1,14 @@
 package fr.insee.pogues.persistence.repository;
 
-public interface MappingCodesListRegistreRepository {
+import fr.insee.pogues.domain.entity.db.MappingCodesListRegistreDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MappingCodesListRegistreRepository
+        extends JpaRepository<MappingCodesListRegistreDB, Long> {
+
+    Optional<MappingCodesListRegistreDB> findByPoguesCodesListId(String poguesId);
 }
