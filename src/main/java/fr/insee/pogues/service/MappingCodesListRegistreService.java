d262aaf5fd9669a3aa45628012d24f4b6b360008
RemiVerriez <remi.verriez@insee.fr>
2026-05-07 15:07:18 +0200

    feat: update mapping pogues registre service

--- src/main/java/fr/insee/pogues/service/MappingCodesListRegistreService.java
+++ src/main/java/fr/insee/pogues/service/MappingCodesListRegistreService.java
@@ -1,4 +1,35 @@
 package fr.insee.pogues.service;
 
+import fr.insee.pogues.domain.entity.db.MappingCodesListRegistreDB;
+import fr.insee.pogues.persistence.repository.MappingCodesListRegistreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class MappingCodesListRegistreService {
+
+    private final MappingCodesListRegistreRepository repository;
+
+    public MappingCodesListRegistreDB create(String poguesId, UUID registreId) {
+
+        repository.findByPoguesCodesListId(poguesId)
+                .ifPresent(m -> {
+                    throw new RuntimeException("Mapping already exists");
+                });
+
+        MappingCodesListRegistreDB entity = new MappingCodesListRegistreDB();
+        entity.setPoguesCodesListId(poguesId);
+        entity.setRegistreCodesListId(registreId);
+
+        return repository.save(entity);
+    }
+
+    public List<MappingCodesListRegistreDB> getAll() {
+        return repository.findAll();
+    }
 }
