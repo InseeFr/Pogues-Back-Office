16aa0c36abb61615dd7a44e74f8544ba9bbf4ac1
RemiVerriez <remi.verriez@insee.fr>
2026-05-07 16:58:58 +0200

    feat: pogues registre codeslist mapping controller

--- src/main/java/fr/insee/pogues/controller/MappingCodesListRegistreController.java
+++ src/main/java/fr/insee/pogues/controller/MappingCodesListRegistreController.java
@@ -1,4 +1,38 @@
 package fr.insee.pogues.controller;
 
+import fr.insee.pogues.domain.entity.db.MappingCodesListRegistreDB;
+import fr.insee.pogues.service.MappingCodesListRegistreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/mapping-codeslist")
+@RequiredArgsConstructor
 public class MappingCodesListRegistreController {
+
+    private final MappingCodesListRegistreService service;
+
+    @PostMapping
+    public ResponseEntity<MappingCodesListRegistreDB> create(
+            @RequestBody Map<String, String> body) {
+
+        String poguesCodesListId = body.get("poguesCodesListId");
+        UUID registreCodesListId = UUID.fromString(body.get("registreCodesListId"));
+
+        MappingCodesListRegistreDB mapping =
+                service.create(poguesCodesListId, registreCodesListId);
+
+        return ResponseEntity.ok(mapping);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MappingCodesListRegistreDB>> getAll() {
+
+        return ResponseEntity.ok(service.getAll());
+    }
 }
