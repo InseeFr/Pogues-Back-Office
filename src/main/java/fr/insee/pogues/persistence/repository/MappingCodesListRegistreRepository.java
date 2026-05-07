1c0539a7c45aefff6af6e28f3eacee54e0636616
RemiVerriez <remi.verriez@insee.fr>
2026-05-07 17:06:13 +0200

    refacotr: rename id

--- src/main/java/fr/insee/pogues/persistence/repository/MappingCodesListRegistreRepository.java
+++ src/main/java/fr/insee/pogues/persistence/repository/MappingCodesListRegistreRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface MappingCodesListRegistreRepository
         extends JpaRepository<MappingCodesListRegistreDB, Long> {
 
-    Optional<MappingCodesListRegistreDB> findByPoguesCodesListId(String poguesId);
+    Optional<MappingCodesListRegistreDB> findByPoguesCodesListId(String poguesCodesListId);
 }
